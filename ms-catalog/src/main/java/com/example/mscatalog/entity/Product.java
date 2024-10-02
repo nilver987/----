e80fed3a8bed132b2b2fa54a623b31b71fa9a4ad
nilver987 <nilver.1967@gmail.com>
2024-10-02 18:23:26 -0500

    first commit

--- ms-catalog/src/main/java/com/example/mscatalog/entity/Product.java
+++ ms-catalog/src/main/java/com/example/mscatalog/entity/Product.java
@@ -1,4 +1,22 @@
 package com.example.mscatalog.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    private String code;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Category category;
+
 }
+
