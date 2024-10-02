71c78ab0fe6b664fa5976848b700f2124d829def
nilver987 <nilver.1967@gmail.com>
2024-10-02 18:44:35 -0500

    first commit

--- ms-provider/src/main/java/com/example/msprovider/MsProviderApplication.java
+++ ms-provider/src/main/java/com/example/msprovider/MsProviderApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsProviderApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MsProviderApplication.class, args);
     }
-
 }
