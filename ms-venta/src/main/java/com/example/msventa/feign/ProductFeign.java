63c2cbd6b5aeaa7fb162aae0a2e6fa83a2aa15c5
nilver987 <nilver.1967@gmail.com>
2024-10-02 22:55:09 -0500

    first commit

--- ms-venta/src/main/java/com/example/msventa/feign/ProductFeign.java
+++ ms-venta/src/main/java/com/example/msventa/feign/ProductFeign.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
+
 @FeignClient(name = "ms-catalog-service" ,path = "/product")
 public interface ProductFeign {
     @GetMapping("/{id}")
