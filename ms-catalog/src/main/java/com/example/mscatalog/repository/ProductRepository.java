9abfda1ae3a4448e0adab6f627235c23a9c808af
nilver987 <nilver.1967@gmail.com>
2024-10-02 22:45:04 -0500

    first commit

--- ms-catalog/src/main/java/com/example/mscatalog/repository/ProductRepository.java
+++ ms-catalog/src/main/java/com/example/mscatalog/repository/ProductRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> { List<Product> findByCode(String name);
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    
 }
